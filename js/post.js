ef0e22d20338a4916ddaf5cd58edd1a69bd7bae3
Akinuliola Akinbobola <akinuliola_a@yahoo.com>
2021-11-01 11:54:43 +0100

    api

---
+++ js/post.js
@@ -0,0 +1,44 @@
+function getPost() {
+    let postId = localStorage.getItem('postNumber')
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data)
+        let postBody = document.getElementById('post-body')
+        let html =""
+        let none = ""
+
+        html += `
+        <div>
+            <div class="card">
+                <div class="card-body">
+                    <div class=" d-flex justify-content-end">
+                    <h6 class="text-danger">${data.id}</h6>
+                    </div>
+                    <h5 class="post-title mb-4">${data.title}</h5>
+                    <p class="post-body">${data.body}</p>
+                </div>
+            </div>
+        </div>
+        `
+
+        none += `
+        <div>
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="post-title mb-4 text-danger">Erorr 404</h3>
+                    <p class="post-body">Page not found</p>
+                </div>
+            </div>
+        </div>
+        `
+         if(Object.keys(data).length !== 0) {
+            postBody.innerHTML = html
+         } else {
+            postBody.innerHTML = none
+         }
+        
+    })
+}
+
+getPost()
